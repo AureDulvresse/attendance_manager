418bc7317062580a3c51005105e04a9f59f04869
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-12-04 12:29:58 +0100

    .

--- attendance_manager/urls.py
+++ attendance_manager/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-  path('employee/', include('employee.urls')),
+    path('', include('employee.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
